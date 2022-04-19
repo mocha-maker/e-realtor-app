d6dff78c34d1730fdfce944e9df178c3835143c3
mocha-maker <flynn.pyc@gmail.com>
2022-04-19 17:29:15 -0400

    Initial Commit
    Create-react-app, update README.md, add firebase config, clean filler files, update font and primary color in index.css

---
+++ src/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFireStore } from 'firebase/firestore'
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCMr27RNlt-WUL62HspoPmAbUfZrQwSQ-E",
+  authDomain: "e-realtor-app.firebaseapp.com",
+  projectId: "e-realtor-app",
+  storageBucket: "e-realtor-app.appspot.com",
+  messagingSenderId: "846300448897",
+  appId: "1:846300448897:web:f1115e0d7b87772b5c6580",
+  measurementId: "G-6J0RC5NBYY"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFireStore()

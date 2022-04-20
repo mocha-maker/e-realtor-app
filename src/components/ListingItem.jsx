59bf500e32d43cbd65e3547485667d9b19790c31
mocha-maker <flynn.pyc@gmail.com>
2022-04-20 13:46:20 -0400

    Create Category pages with listings list

---
+++ src/components/ListingItem.jsx
@@ -0,0 +1,64 @@
+import { Link } from 'react-router-dom'
+import { ReactComponent as DeleteIcon } from '../assets/svg/deleteIcon.svg'
+import bedIcon from '../assets/svg/bedIcon.svg'
+import bathtubIcon from '../assets/svg/bathtubIcon.svg'
+
+function ListingItem({ listing, id, onDelete }) {
+
+  // deconstruct listing
+  const {
+    type,
+    name,
+    imageUrls,
+    location,
+    offer,
+    discountedPrice,
+    regularPrice,
+    bedrooms,
+    bathrooms,
+    furnished
+  } = listing
+
+  return (
+    <li className="categoryListing">
+      <Link to={`/category/${type}/${id}`} className='categoryListingLink'>
+        <img 
+          className='categoryListingImg'
+          src={imageUrls[0]} alt={name} />
+          <div className="categoryListingDetails">
+            <p className="categoryListingLocation">{location}</p>
+            <p className="categoryListingName">{name}</p>
+            
+            <p className="categoryListingPrice">
+              ${(offer ? discountedPrice : regularPrice)
+                  .toString().replace(/\B(?=(\d{3})+(?!\d))/g,',')} 
+                {type === 'rent' && ' / Month' }
+            </p>
+            <div className="categoryListingInfoDiv">
+              <img src={bedIcon} alt='bed'/>
+              <p className='categoryListingInfoText'>
+              {bedrooms} {bedrooms > 1 ? 'Bedrooms' : 'Bedroom'}
+              </p>
+              <img src={bathtubIcon} alt='bathtub'/>
+              <p className='categoryListingInfoText'>
+              {bathrooms} {bathrooms > 1 ? 'Bathrooms' : 'Bathroom'}
+              </p>
+            </div>
+          </div>
+      </Link>
+
+      {
+        // If owned, allow delete
+        onDelete && (
+        <DeleteIcon 
+          className='removeIcon'
+          fill='red'
+          onClick={() => onDelete(id, name)}
+        />
+        )
+      }
+
+    </li>
+  )
+}
+export default ListingItem
--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,6 +1,101 @@
+import { useState, useEffect, useRef } from 'react'
+import { useParams } from 'react-router-dom'
+import {
+  collection,
+  getDocs,
+  query,
+  where,
+  orderBy,
+  limit,
+  startAfter,
+} from 'firebase/firestore'
+import { db } from '../firebase.config'
+import { toast } from 'react-toastify'
+import Spinner from '../components/Spinner'
+import ListingItem from '../components/ListingItem'
+
 function Category() {
+  // States
+  const [listings, setListings] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  // Fetching toast to prevent duplicates
+  const notify = () => {
+    toast.loading("Fetching listings", {toastId: "fetchToast"})
+  }
+
+  const params = useParams()
+
+  // Retrieve data from db on Load
+  useEffect(() => {
+    const fetchListings = async () => {
+      try {
+        notify()
+        
+        // Get collection reference
+        const listingsRef = collection(db, 'listings')
+
+        // Create a query (collection field, query type, query)
+        const q = query(
+          listingsRef,
+          where('type', '==', params.categoryName),
+          orderBy('timestamp', 'desc'),
+          limit(10)
+        )
+
+        // Execute query
+        const querySnap = await getDocs(q)
+
+        // Iterate through snapshot
+
+        const results = []
+
+        querySnap.forEach((doc) => {
+          return results.push({
+            id: doc.id,
+            data: doc.data(),
+          })
+        })
+        setListings(results)
+        setLoading(false)
+        toast.dismiss()
+
+      } catch (error) {
+        console.log(error);
+        toast.dismiss()
+        toast.error("Could not fetch listings")
+      }
+    }
+
+    // Run fetch listings
+    fetchListings()
+  }, [params.categoryName])
+
   return (
-    <div>Category</div>
+    <div className='category'>
+      <header className='pageContainer'>
+        <p className='pageHeader'>Places for {params.categoryName === 'rent' ? 'Rent' : 'Sale'}</p>
+      </header>
+      
+      { loading ? (
+      <Spinner/>
+      ) : listings && listings.length > 0 ? (
+        <>
+          <main>
+            <ul className="categoryListings">
+              {listings.map((listing) => (
+                <ListingItem 
+                  listing={listing.data} 
+                  id={listing.id}
+                  onDelete={true}/>              
+              ))}
+            </ul>
+          </main>
+        </>
+      ) : (
+        <p>No listings available.</p>
+      )}
+    </div>
   )
 }
 export default Category
